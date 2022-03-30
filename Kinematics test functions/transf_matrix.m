e7c5d9be6f15f0dfd9038f6289fe6f5f686a3c85
joaolobatopereira117 <101321779+joaolobatopereira117@users.noreply.github.com>
2022-03-30 09:33:06 +0100

    Delete Kinematics test functions directory

--- Kinematics test functions/transf_matrix.m
+++
@@ -1,8 +0,0 @@
-function [T] = transf_matrix (alpha, a, theta_i, d_i)
-%Performes Homogeneus trasnformation matrix
-
-T = [cos(theta_i)            -sin(theta_i)             0           a;
-     sin(theta_i)*cos(alpha) cos(theta_i)*cos(alpha) -sin(alpha) -sin(alpha)*d_i;
-     sin(theta_i)*sin(alpha) cos(theta_i)*sin(alpha) cos(alpha)  cos(alpha)*d_i;
-     0                       0                       0           1             ];
-end
